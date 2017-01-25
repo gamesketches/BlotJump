4989e39c6476cf92a36b0539ad967a8aeb20b596
gamesketches <sam.von.ehren@gmail.com>
2017-01-25 14:08:40 -0500

    Camera rotation lock script

---
+++ Assets/CameraRotationLock.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRotationLock : MonoBehaviour {
+
+	Quaternion myRotation;
+	// Use this for initialization
+	void Start () {
+		myRotation = transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.rotation = myRotation;
+	}
+}
